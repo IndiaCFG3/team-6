33089a61913619e028d7ea52cdb7dbcc8f48768b
Suraj-Kaple <53992587+Suraj-Kaple@users.noreply.github.com>
2020-08-16 18:30:28 +0530

    Add files via upload

--- new_course.js
+++ new_course.js
@@ -1,1 +1,6 @@
+var course_name = $("#course_name").value;
+var author_name = $("#author").value;
+var course_id = $("#ID").value;
+var description = $("#description").value;
 
+// alert(course_name);
